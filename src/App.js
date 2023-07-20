7a251f4714f183e49b7419a9143bfefefd64dc31
laxmichelluri <laxmichelluri744@gmail.com>
2023-07-20 08:22:44 +0000

    authentication-react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Switch, Route} from 'react-router-dom'
+import {Component} from 'react'
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import NotFound from './components/NotFound'
+import './App.css'
+
+class App extends Component {
+  render() {
+    ;<div className="app-container">
+      <div className="body-section">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  }
+}
+
+export default App
